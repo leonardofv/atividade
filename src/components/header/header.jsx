0a395704406f66a3daccc234f60e420b842507bd
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-03-19 15:19:23 -0300

    editando botões na header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -19,6 +19,11 @@ export default function Header() {
                 <img className={S.logo} src={Logo} alt="logo" />
                 <h1 className={S.title}>CyberSegurança</h1>
                 <p className={S.info}>Protegendo você contra os golpes digitais</p>
+
+                <div className={S.search}>
+                    <input className={S.input} type='text' placeholder='Pesquisar golpes'/>
+                    {/* <img className={S.icon} src={Search} alt="" /> */}
+                </div>
         
                 <nav className={S.ContainerCategory}>
                     <ul className={S.categories}>

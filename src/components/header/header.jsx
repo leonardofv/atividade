e08ebb70565851cf0c0d03e729cdce886f152281
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-03-22 09:41:57 -0300

    alteração nos Links da header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,42 +1,54 @@
 import S from './header.module.scss';
 import Logo from '../../assets/escudo.png';
-// import Search from '../../assets/search_input.png';
-
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Home from '../../pages/home/home';
 import Phishing from '../../pages/phishing/phishing';
 import Ransomware from '../../pages/ransomware/ransomware';
-import GolpeRedeSocial from '../../pages//golpeRedeSocial/golpeRedeSocial';
+import GolpeRedeSocial from '../../pages/golpeRedeSocial/golpeRedeSocial';
 import FraudeBancaria from '../../pages/fraudeBancaria/fraudeBancaria';
 import GolpeCompras from '../../pages/golpesCompras/golpeCompras';
 import RouboIdentidade from '../../pages/rouboIdentidade/rouboIdentidade';
 
-export default function Header() {
-    return (
-        <BrowserRouter>
-            <header className={S.container}>
-                <img className={S.logo} src={Logo} alt="logo" />
-                <h1 className={S.title}>CyberSegurança</h1>
-                <p className={S.info}>Protegendo você contra os golpes digitais</p>
+function Header() {
+    const location = useLocation();
 
-                {/* <div className={S.search}> */}
-                    {/* <input className={S.input} type='text' placeholder='Pesquisar golpes'/> */}
-                    {/* <img className={S.icon} src={Search} alt="" /> */}
-                {/* </div> */}
-        
-                <nav className={S.ContainerCategory}>
-                    <ul className={S.categories}>
-                        <li className={S.category}><Link className={S.Link} to="/">Home</Link></li>
-                        <li className={S.category}><Link className={S.Link} to="/phishing">Phishing</Link></li>
-                        <li className={S.category}><Link className={S.Link}  to="/ransomware">Ransomware</Link></li>
-                        <li className={S.category}><Link className={S.Link}  to="/golpeRedeSocial">Redes sociais</Link></li>
-                        <li className={S.category}><Link className={S.Link}  to="/fraudeBancaria">Fraudes bancárias</Link></li>
-                        <li className={S.category}><Link className={S.Link}  to="/golpeCompras">Compras online</Link></li>
-                        <li className={S.category}><Link className={S.Link}  to="/rouboIdentidade">Roubo de identidade</Link></li>
-                    </ul>
-                </nav>
-            </header>
+    const menuItems = [
+        { path: '/phishing', label: 'Phishing' },
+        { path: '/ransomware', label: 'Ransomware' },
+        { path: '/golpeRedeSocial', label: 'Redes sociais' },
+        { path: '/fraudeBancaria', label: 'Fraudes bancárias' },
+        { path: '/golpeCompras', label: 'Compras online' },
+        { path: '/rouboIdentidade', label: 'Roubo de identidade' },
+    ];
+
+    return (
+        <header className={S.container}>
+            <img className={S.logo} src={Logo} alt="logo" />
+            <h1 className={S.title}>CyberSegurança</h1>
+            <p className={S.info}>Protegendo você contra os golpes digitais</p>
+
+            <nav className={S.ContainerCategory}>
+                <ul className={S.categories}>
+                    {menuItems.map((item) => {
+                        const isActive = location.pathname === item.path;
+                        return (
+                            <li className={S.category} key={item.path}>
+                                <Link className={S.Link} to={isActive ? '/' : item.path}>
+                                    {isActive ? 'Home' : item.label}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default function App() {
+    return (
+        <Router>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/phishing" element={<Phishing />} />
@@ -46,6 +58,6 @@ export default function Header() {
                 <Route path="/golpeCompras" element={<GolpeCompras />} />
                 <Route path="/rouboIdentidade" element={<RouboIdentidade />} />
             </Routes>
-        </BrowserRouter>
-   )
+        </Router>
+    );
 }
